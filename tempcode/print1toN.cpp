dcd4aca3dddeb235febabd10e0c0f374cbfc3209
MurayPanic <44858565+MurayPanic@users.noreply.github.com>
2019-05-29 11:48:30 +0800

    Update print1toN.cpp

--- tempcode/print1toN.cpp
+++ tempcode/print1toN.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include <typeinfo>
 using namespace std;
 
 
@@ -11,20 +12,25 @@ bool Increment(string& number){
      int nTakeOver =0;
      int nLength = number.size();
 
-     for (int i =nLength; i>=0; i--){
+     for (int i =nLength-1; i>=0; i--){
+		  //string cur_digit_str (number[i]);
+		  int cur_digit = number[i]-'0';
      
-          int nSum = number[i]+1;
-	  cout<< "nSUm:"<<nSum<<endl;
+          int nSum = cur_digit+1;
+		  //cout<<"cur_digit:"<<cur_digit<<endl;
+	      //cout<< "nSUm:"<<nSum<<endl;
          
           
          if (nSum <10){
              string newDigit = to_string(nSum) ;
-             number.replace(number.begin()+i-1, number.begin()+i, newDigit);
+			 //cout<< "newDigit: "<< newDigit<<endl;
+			 //cout<<"newDigit type: "<<typeid(newDigit).name()<<endl;
+             number.replace(i, 1, newDigit);
              return isOverflow;
              
           }else{
              string newDigit = to_string(nTakeOver) ;
-             number.replace(number.begin()+i, number.begin()+i+1, newDigit);       
+             number.replace(i, 1, newDigit);       
           }
           
      }
@@ -43,31 +49,56 @@ void test_Increment(){
      inputNumber[3]-'0'+'6';
      cout<<inputNumber<<endl;
      
-     string testInput(3,'0');
-     cout<<testInput<<endl;
+     string testInput(1,'0');
+     /*cout<<testInput<<endl;
      Increment(testInput);
      cout<<testInput<<endl;
      Increment(testInput);
      cout<<testInput<<endl;
      Increment(testInput);
      cout<<testInput<<endl;
-
+	 */
+	 
+	 for(int i =0; i<10; ++i){
+		 bool overflowOrNot=Increment(testInput);
+         cout <<"Overflow:"<<overflowOrNot<<endl;		 
+		cout<<testInput<<endl;
+	 }
+	 
+	 string testInput2(3,'0');
+	 
+	 while(not Increment(testInput2)){
+		 cout<< testInput2<<endl;
+	 }
 
 
 }
 
 
 void PrintNumber(string number){
-      cout<<endl;
+	  bool notZero{false};
+	  for(auto digit : number){
+		  
+		  if(!notZero && digit=='0'){continue;}
+		  else{notZero =true;}
+		  cout <<digit;
+	  }
+	  cout<<endl;
       
 }
 
+
+void test_PrintNumber(){
+	string a("00010240309");
+	PrintNumber(a);
+	
+}
 
 
 void print1ToMaxofNDigits(int n){
 	if(n<=0){return ;}
         string number(n, '0');
-	number.push_back('\0');
+	//number.push_back('\0');
 
     while(!Increment(number) ){
     PrintNumber(number);
@@ -89,7 +120,7 @@ void execute(){
 
 
 int main(){
-    test_Increment();
+    execute();
 
     return 0;
 }
